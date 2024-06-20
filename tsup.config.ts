fcf9a17b550ec23b2211c01d2103c3a7c9dc781b
Ram-Amoncar <ramamonkar444@gmail.com>
2024-06-20 17:13:48 +0530

    build(MAJOR): tuned up tsup & package config - (tsup) removed sourcemap - (tsup) license banner - (tsup) support for iife - (package) added types, main, module & exports paths

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,46 @@
 import { defineConfig } from 'tsup'
+import type { NormalizedOptions, Format } from 'tsup'
+
+interface Context {
+    options: NormalizedOptions
+    format: Format
+    /** "type" field in project's package.json */
+    pkgType?: string
+}
+
+interface Result {
+    js?: string
+}
+
+function outputExtensions(ctx: Context): Result {
+    if (ctx.format === 'cjs') return { js: '.js' }
+    if (ctx.format === 'esm') return { js: '.mjs' }
+    return { js: '.min.js' }
+}
+
+// You can put license here.
+// Example
+const license = '/* This is Unlicensed */'
 
 export default defineConfig({
-    entry: ['src/index.ts'],
-    format: ['cjs', 'esm'], // Build for commonJS and ESmodules
-    dts: true, // Generate declaration file (.d.ts)
     splitting: false,
-    sourcemap: true,
+    entry: ['src/index.ts'],
+    // Generate declaration file (.d.ts & .d.mts)
+    // And adds banner
+    dts: {
+        banner: license
+    },
+    // Builds for commonJS, ESmodules & Browser
+    format: ['cjs', 'esm', 'iife'],
+    outExtension: outputExtensions,
+    // Cleaning './dist/' before building
     clean: true,
-    minify: true
+    // Header/Banner
+    banner: {
+        js: license
+    },
+    // Minify Options
+    minifyIdentifiers: false,
+    minifySyntax: true,
+    minifyWhitespace: true
 })
